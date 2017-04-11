e79929d0bef3c08662e16c46cae3af38431e9993
downgoon <downgoon@qq.com>
2017-04-11 19:16:19 +0800

    package shell script

---
+++ fpm-mac.sh
@@ -0,0 +1,1 @@
+fpm -s dir -t osxpkg -n gbk2utf8 -v 0.1.1 -C /opt/packages --prefix /usr/local --after-install gbk2utf8/share/after_install.sh
---
+++ gbk2utf8/share/after_install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ln -sf /usr/local/gbk2utf8/bin/gbk2utf8 /usr/local/bin/gbk2utf8
